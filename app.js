799da9bb3d564953808442782d32309319cdd584
AfrozaIslam <70200297+AfrozaIslam@users.noreply.github.com>
2020-09-20 09:58:01 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.btn1').click(function(){
+
+
+        $('p').hide('slow')
+
+
+    });
+
+
+    $('.btn2').click(function(){
+        $("p").show('slow')
+
+    })
+
+});
